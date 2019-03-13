53944e13fe54bc27d5c3b1ab1b4b9976099a5bbe
Gabriela Roa <gabriela@Gabrielas-MacBook-Pro.local>
2019-03-13 17:56:33 -0500

    add new slide out navigation bar

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-var sliderTrigger = document.getElementsByClassName("slider-trigger")[0];
-var slider = document.getElementsByClassName('slider-parent')[0];
-sliderTrigger.addEventListener( "click" , function(el){
-if(slider.classList.contains("active")){
-  slider.classList.remove("active");
- }else{
-  slider.classList.add("active");
- }
-});
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
